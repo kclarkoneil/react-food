1f6aa93923f65953cc0b7b0d48ccbc814db13a3d
Kit <kit@Kits-MacBook-Pro.local>
2022-03-18 14:52:32 -0700

    Search bar complete axios package installed

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,20 +1,40 @@
 import React from 'react'
 import {View, Text, StyleSheet} from 'react-native'
-
-const SearchBar = () => {
+import { Feather } from '@expo/vector-icons'
+const SearchBar = ({term, onTermChange}) => {
     return (
-        <View style={styles.background}>
-            <Text>Search Screen</Text>
+        <View style={styles.backgroundStyle}>
+            <Feather style={styles.iconStyle} name="search" size={30}/>
+            <TextInput 
+            autoCapitalize="none"
+            autoCorrect={false}
+            style={styles.inputStyle} 
+            placeholder= "Search"
+            value = {term}
+            onChangeText = {newTerm => onTermChange(newTerm)}
+             />
+            
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    background: {
+    backgroundStyle: {
         backgroundColor: '#F0EEEE',
         height: 50,
         borderRadius: 5,
-        marginHorizontal: 50
+        marginHorizontal: 50,
+        flexDirection: 'row'
+    },
+    inputStyle: {
+        
+        flex: 1,
+        fontSize: 18
+    },
+    iconStyle: {
+        fontSize: 35,
+        alignSelf: 'center',
+        marginHorizontal: 15
     }
 })
 
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {View, Text, StyleSheet} from 'react-native'
 import SearchBar from '../Components/SearchBar'
 const SearchScreen = () => {
+    const [term, setTerm] = useState('')
     return (
         <View>
-            <Text>Search Screen</Text>
+            <SearchBar term={term} onTermChange={newTerm => setTerm(newTerm)}/>
         </View>
     )
 }
